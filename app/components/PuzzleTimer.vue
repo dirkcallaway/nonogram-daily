1a5bb69367e8d46bfbe82def98a8695899d2fea6
Matt Perroni <matt.perroni@gmail.com>
2026-03-03 08:33:00 -0700

    Added puzzletimer

---
+++ app/components/PuzzleTimer.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+  display: string
+  solved: boolean
+}>()
+</script>
+
+<template>
+  <div class="flex items-center gap-2 text-lg font-mono">
+    <span :class="solved ? 'text-green-500' : 'text-gray-700 dark:text-gray-300'">
+      {{ display }}
+    </span>
+    <span v-if="solved" class="text-green-500 text-sm font-sans font-semibold">
+      Solved!
+    </span>
+  </div>
+</template>
